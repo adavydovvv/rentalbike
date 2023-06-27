91342139bb9771ad0bf01e277978de2201a369d6
Artyom <adavydovvv@mail.ru>
2023-06-27 20:27:31 +0300

    Первые результаты

--- src/main/java/com/example/bike_rent/HelloController.java
+++ src/main/java/com/example/bike_rent/HelloController.java
@@ -1,14 +1,54 @@
 package com.example.bike_rent;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authorizebutton;
+
+    @FXML
+    private TextField loginfield;
+
+    @FXML
+    private Button mainregbutton;
+
+    @FXML
+    private PasswordField passwordfield;
+
+    @FXML
+    void initialize() {
+        mainregbutton.setOnAction(actionEvent -> {
+            mainregbutton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("registration.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.bike_rent {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
 
 
     opens com.example.bike_rent to javafx.fxml;
